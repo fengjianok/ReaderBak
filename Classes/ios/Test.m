4827071a099ffc35c14f2a279b8dfcbe1a3280db
fengjianok <fengjianok@gmail.com>
2013-05-02 16:45:08 +0800

    test

---
+++ Classes/ios/Test.m
@@ -0,0 +1,18 @@
+//
+//  Test.m
+//  ReaderBak
+//
+//  Created by zheng libao on 13-5-2.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+- (void)println 
+{
+    NSLog(@"Hello world!");
+}
+
+@end
---
+++ Project/ReaderBak/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReaderBak
+//
+//  Created by zheng libao on 13-5-2.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
